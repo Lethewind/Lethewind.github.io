bcb7c6d4e22880014dcf9ef112a90392cec0da02
lethewind <farewellswind@foxmail.com>
2026-04-20 20:40:37 +0800

    fix: update astro config site and base for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,8 @@ import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://denglin.github.io',
+	site: 'https://Lethewind.github.io',
+	base: '/denglin.github.io',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
